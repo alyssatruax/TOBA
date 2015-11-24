5892137318f7df74643d210f56abb9a537ad680a
alyssatruax <alyssatruax@hotmail.com>
2015-11-23 20:36:38 -0500

    Assignment 3

---
+++ src/java/TOBA/data/AccountDB.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TOBA.data;
+
+import TOBA.business.Account;
+import TOBA.business.User;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+public class AccountDB {
+    
+    public static void insert (Account account) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();
+        
+        try {
+            em.persist(account);
+            trans.commit();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            trans.rollback();
+        }
+        finally {
+            em.close();
+        }
+    }
+    
+        public static Account selectAccount(long userID) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        String qString = "SELECT a FROM Account a " +
+                "WHERE a.userID = :userID";
+        
+        TypedQuery<Account> q = em.createQuery(qString, Account.class);
+        q.setParameter("userID", userID);
+        
+        Account account = null;
+        
+        try {
+            account = q.getSingleResult();
+        }
+        catch (NoResultException e) {
+            System.out.println(e);
+        }
+        finally {
+            em.close();
+        }
+        return account;
+    }
+    
+    
+    
+}
